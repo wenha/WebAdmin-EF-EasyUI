49ffbd751eb21e211d2f1190ebb7c402ce649a90
Wenha Yao <ywh_wh@126.com>
2019-03-05 22:52:12 +0800

    添加easyui样式

--- App.Web/Global.asax.cs
+++ App.Web/Global.asax.cs
@@ -16,6 +16,9 @@ namespace App.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //启用压缩
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
